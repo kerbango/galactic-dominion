fe9362e597288b603d3b017a8a1ba322e35222df
kerbango <kerbango@proton.me>
2026-08-31 13:07:29 +0000

    Register Phase Scout artwork

--- src/data/shipArt.js
+++ src/data/shipArt.js
@@ -7,6 +7,7 @@ const SHIP_ART = {
   light_scout: { art: 'https://media.base44.com/images/public/6a8dedaa90af486a558f758e/fa07a0737_lightscout.png' },
   medium_scout: { art: 'https://media.base44.com/images/public/6a8dedaa90af486a558f758e/e5dbf0c69_mediumscout.png' },
   heavy_scout: { art: 'https://media.base44.com/images/public/6a8dedaa90af486a558f758e/c4aa71c97_heavyscout.png' },
+  phase_scout: { art: 'https://media.base44.com/images/public/6a8dedaa90af486a558f758e/70f1e681a_ChatGPTImageAug31202609_06_09AM.png' },
 };
 
 export function getShipArt(unitType) {
